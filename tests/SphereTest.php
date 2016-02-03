f541f86cc8c5dbb6f6598a6760d79cb6d66fc225
Arnel Pancho Jorojoro <arnacmj@gmail.com>
2016-02-04 05:15:40 +0800

    Update SphereTest.php

--- tests/SphereTest.php
+++ tests/SphereTest.php
@@ -1,17 +1,25 @@
-<!--@todo-->
-
 <?php
+
+include_once('src/Sphere.php');
 
 class SphereTest extends\PHPUnit_Framework_TestCase {
 
 	public function testGetTheAreaOfSphere()
 	{
-		// @todo
+		$radius = 5;
+		$result = 4 * pi() * sqrt($radius);
+		$area_sphere = new Shape/Sphere($radius);
+		
+		$this->assertEquals($result, $area_sphere);
 	}
 	
 	public function testGetTheVolumeOfSphere()
 	{
-	 // @todo
+	 	$radius = 4;
+	 	$result = 4 / 3 * pi() * sqrt($radius) * $radius;
+	 	$volume_sphere = new Shape/Sphere($radius);
+	 	
+	 	$this->assertEquals($result, $volume_sphere);
 	}
 
 }
